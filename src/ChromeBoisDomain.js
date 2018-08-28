113554322daf9255d91eaf7707bea09d24d4fd32
Katleiah Ramos <katleiahramos@gmail.com>
2018-08-28 01:36:31 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -29,7 +29,7 @@ export default class ChromeBoisDomain extends Component {
   render() {
     return (
       <canvas
-        onKeyPress={this.handleKeyDown }
+        onKeyPress={this.handleKeyDown}
         onClick={() => toggleCycling()}
         onMouseMove={this.handleMouseMove}
         width='900'
